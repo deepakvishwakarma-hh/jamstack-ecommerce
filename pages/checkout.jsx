01217f3cb1ee5c71fce10dbd5638f84b57dd9560
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-09-07 11:39:20 +0530

    make payment is now async

--- pages/checkout.jsx
+++ pages/checkout.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import makePayment from '../utils/payment'
+import makePayment from '../utils/razor-payment'
 import CartLink from '../components/CartLink'
 import DENOMINATION from '../utils/currencyProvider'
 import { useState, useEffect, useContext } from 'react'
