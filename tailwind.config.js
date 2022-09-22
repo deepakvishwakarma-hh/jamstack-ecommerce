1a70aef1ee405a68d0ce75d3bcd0608a0aac29b9
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-09-22 14:33:32 +0530

    Revert "TTR  : Reorder"
    This reverts commit f4bcf856875c276981d63bdd8be944751cfd1edd.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,5 @@
 module.exports = {
-  plugins: [
-    "tailwindcss",
-    [
-      "@fullhuman/postcss-purgecss",
-      process.env.NODE_ENV === "production"
-        ? {
-          // the paths to all template files
-          content: [
-            "./pages/**/*.{js,jsx,ts,tsx}",
-            "./components/**/*.{js,jsx,ts,tsx}",
-          ],
-          // function used to extract class names from the templates
-          defaultExtractor: (content) =>
-            content.match(/[\w-/:]+(?<!:)/g) || [],
-        }
-        : false,
-    ],
-  ],
-  // purge: ['./pages/**/*.js', './components/**/*.js'],
+  purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -195,5 +177,22 @@ module.exports = {
       }
     },
   },
-
+  plugins: [
+    "tailwindcss",
+    [
+      "@fullhuman/postcss-purgecss",
+      process.env.NODE_ENV === "production"
+        ? {
+          // the paths to all template files
+          content: [
+            "./pages/**/*.{js,jsx,ts,tsx}",
+            "./components/**/*.{js,jsx,ts,tsx}",
+          ],
+          // function used to extract class names from the templates
+          defaultExtractor: (content) =>
+            content.match(/[\w-/:]+(?<!:)/g) || [],
+        }
+        : false,
+    ],
+  ],
 }
