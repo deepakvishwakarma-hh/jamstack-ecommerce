1143ecf282e7df6b1bb281e7d504ff55230d4352
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-09-28 11:24:19 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === "production",
-    content: ['./pages/**/*.js', './components/**/*.js', './layouts/**/*.js']
-  },
+  // purge: {
+  //   enabled: process.env.NODE_ENV === "production",
+  //   content: ['./pages/**/*.js', './components/**/*.js', './layouts/**/*.js']
+  // },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
